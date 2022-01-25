d69757d238369104512913ef17859e0194d88967
InveterateCoder <inveterate.coder@gmail.com>
2022-01-25 13:51:43 +0300

    finishing

--- src/controllers/listController.js
+++ src/controllers/listController.js
@@ -1,4 +1,5 @@
 const { request, response } = require("express");
+const { getDb } = require("../miscellaneous/db");
 
 
 /**
@@ -6,9 +7,26 @@ const { request, response } = require("express");
  * @param {request} req 
  * @param {response} res 
  */
-function listController(req, res) {
-    console.log(req.body);
-    res.send("hello");
+async function listController(req, res) {
+    let { name } = req.body;
+    if (typeof name !== "string")
+        return res.status(400).json({ list: [], error: "name should be a string" });
+    name = name.trim();
+    if (!name) return res.json({ list: [] });
+    const words = name.split(/\s/).filter(w => w);
+
+    const db = await getDb();
+    const { rowCount, rows } = await db.query(
+        `
+            SELECT first_name, last_name FROM users
+            WHERE ${words.map(
+            word => ["first_name", "last_name"].map(
+                rowName => `LOWER(${rowName}) LIKE '%${word.toLowerCase()}%'`)
+                .join(" OR "))
+            .join(" OR ")};
+        `);
+    if (rowCount === 0) return res.json({ list: [] });
+    res.json({ list: rows.map(item => Object.values(item).join(" ")) });
 }
 
 module.exports = listController;
